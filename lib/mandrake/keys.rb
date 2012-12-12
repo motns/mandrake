9386091a246f09330a93bd5353aead3de9af3fc4
Adam Borocz <boroczadam@gmail.com>
2012-12-12 13:45:35 +0000

    Support for additional options for Keys

--- lib/mandrake/keys.rb
+++ lib/mandrake/keys.rb
@@ -20,14 +20,20 @@ module Mandrake
         raise %Q(Key "#{name}" is already defined) if keys.key? name
         raise %Q(Key name "#{name}" is already used as an alias for another field) if aliases.key? name
 
-        field_alias = opt[:as] || name
+        field_alias = (opt[:as] || name).to_sym
 
         raise %Q(Alias "#{field_alias}" already taken) if aliases.key? field_alias
         raise %Q(Alias "#{field_alias}" is already used as a field name) if keys.key? field_alias
 
+        raise %Q(Length option for "#{name}" has to be an Integer or a Range) if opt[:length] && ! (opt[:length].is_a?(Integer) || opt[:length].is_a?(Range))
+
         keys[name] = {
           :type => type,
-          :alias => field_alias
+          :alias => field_alias,
+          :required => opt[:required] || false,
+          :default => opt[:default] || nil,
+          :length => opt[:length] || nil,
+          :format => opt[:format] || nil
         }
 
         aliases[field_alias] = name
