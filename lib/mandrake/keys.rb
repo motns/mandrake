e3e8cfae0719ab3a5f58d1235097d73a579376f1
Adam Borocz <boroczadam@gmail.com>
2012-12-25 15:53:57 +0000

    Support for change tracking in Model attributes

--- lib/mandrake/keys.rb
+++ lib/mandrake/keys.rb
@@ -2,6 +2,50 @@ module Mandrake
   module Keys
     extend ActiveSupport::Concern
 
+    def initialize(attrs = {})
+      @attributes = {}
+      @changed_attributes = {}
+      @previously_changed = {}
+
+
+      # Load defaults
+      self.class.keys.each do |k, v|
+        if attrs.key? k
+          @attributes[k] = attrs[k]
+        else
+          # @TODO - proper defaults here
+          #   * split by pre- and post-processing defaults (flat values vs. Procs)
+          @attributes[k] = nil
+        end
+      end
+    end
+
+
+    def keys
+      self.class.keys
+    end
+
+
+    ########################################
+    # Change tracking
+
+    def changed?
+      not @changed_attributes.empty?
+    end
+
+    def changes
+      return nil if @changed_attributes.empty?
+
+      changes = {}
+
+      @changed_attributes.each do |key, old|
+        changes[key] = [old, @attributes[key]]
+      end
+
+      changes
+    end
+
+
     module ClassMethods
 
       # Document schema definition: field_name => properties
@@ -9,10 +53,12 @@ module Mandrake
         @keys ||= {}
       end
 
+
       # Reverse lookup for matching aliases to field names: alias => field_name
       def aliases
         @aliases ||= {}
       end
+
 
       def key(name, type, opt = {})
         name = name.to_sym
@@ -37,6 +83,55 @@ module Mandrake
         }
 
         aliases[field_alias] = name
+
+
+        ##########################################################
+        # Create field accessors
+        # and also ActiveModel::Dirty tracking stuff
+
+        self.class_eval do
+          # Getter
+          define_method name do
+            @attributes[name]
+          end
+
+          alias_method field_alias, name unless name == field_alias
+
+
+          # Setter
+          field_setter = "#{name}=".to_sym
+          setter_alias = "#{field_alias}=".to_sym
+
+          define_method field_setter do |val|
+            @changed_attributes[name] = @attributes[name]
+            @attributes[name] = val
+          end
+
+          alias_method setter_alias, field_setter unless field_setter == setter_alias
+
+
+          # Change reflection
+          field_changed_method = "#{name}_changed?".to_sym
+          field_change_method = "#{name}_change".to_sym
+          field_was_method = "#{name}_was".to_sym
+
+          define_method field_changed_method do
+            @changed_attributes.key? name
+          end
+
+          # => [original, new]
+          define_method field_change_method do
+            if @changed_attributes.key? name
+              [@changed_attributes[name], @attributes[name]]
+            else
+              nil
+            end
+          end
+
+          define_method field_was_method do
+            @changed_attributes[name]
+          end
+        end
 
       end
     end
