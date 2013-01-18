7824475bce7a4e6442c395eb2adfe1e8efe53794
Adam Borocz <boroczadam@gmail.com>
2013-01-18 14:12:43 +0000

    Type-casting for String Type

--- lib/mandrake/type/string.rb
+++ lib/mandrake/type/string.rb
@@ -2,6 +2,10 @@ module Mandrake
   module Type
   	class String < Base
       param length: 50, format: nil
+
+      def value=(val)
+        @value = val.nil? ? nil : val.to_s
+      end
     end
 	end
 end
