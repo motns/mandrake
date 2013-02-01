2ca3552a45cd4736be363db3d4303df09705469c
Adam Borocz <boroczadam@gmail.com>
2013-02-01 13:50:02 +0000

    Take out ActiveModel altogether for now

--- lib/mandrake/components/validations.rb
+++ lib/mandrake/components/validations.rb
@@ -2,13 +2,9 @@ module Mandrake
   module Validations
     extend ActiveSupport::Concern
 
-    included do |base|
-      base.send :include, ActiveModel::Validations
-    end
-
     module ClassMethods
       def create_validations(key)
-        validates_presence_of key.name if key.required
+        #validates_presence_of key.name if key.required
       end
     end
   end
--- spec/components/validations_spec.rb
+++ spec/components/validations_spec.rb
@@ -1,36 +1,58 @@
 require 'spec_helper'
 
 describe Mandrake::Validations do
+  # context "#valid?" do
+  #   context "when there is a required key" do
+  #     before(:all) do
+  #       @book_class = Class.new(TestBaseModel) do
+  #         key :title, String, required: true
+  #       end
+  #     end
+
+  #     context "defined in the instance" do
+  #       before do
+  #         @book = @book_class.new({:title => "My book"})
+  #       end
+
+  #       it "returns true" do
+  #         @book.valid?.should be_true
+  #       end
+  #     end
+
+  #     context "not defined in the instance" do
+  #       before do
+  #         @book = @book_class.new
+  #       end
+
+  #       it "returns false" do
+  #         @book.valid?.should be_false
+  #       end
+  #     end
+  #   end
+  # end
 
 
-  context "#valid?" do
-    context "when there is a required key" do
-      before(:all) do
-        @book_class = Class.new(TestBaseModel) do
-          key :title, String, required: true
-        end
-      end
+  # context "#errors" do
+  #   context "when there is a required key" do
+  #     before(:all) do
+  #       @book_class = Class.new(TestBaseModel) do
+  #         key :title, String, required: true
+  #       end
+  #     end
 
-      context "defined in the instance" do
-        before do
-          @book = @book_class.new({:title => "My book"})
-        end
+  #     context "not defined in the instance" do
+  #       before do
+  #         @book = @book_class.new
+  #         @book.valid?
+  #       end
 
-        it "returns true" do
-          @book.valid?.should be_true
-        end
-      end
+  #       it "returns error message for blank property" do
+  #         puts @book.errors.inspect
+  #         @book.errors.should include(:title)
+  #         @book.errors[:title].should include("can't be blank")
+  #       end
+  #     end
+  #   end
 
-      context "not defined in the instance" do
-        before do
-          @book = @book_class.new
-        end
-
-        it "returns false" do
-          @book.valid?.should be_false
-        end
-      end
-    end
-  end
-
+  # end
 end
