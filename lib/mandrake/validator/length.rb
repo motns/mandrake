5392a6176b634a36d792ca31d4340108af2f0d59
Adam Borocz <boroczadam@gmail.com>
2013-02-01 17:56:52 +0000

    run_validator methods should be private

--- lib/mandrake/validator/length.rb
+++ lib/mandrake/validator/length.rb
@@ -7,27 +7,29 @@ module Mandrake
         :long => "has to be shorter than %d characters"
       }
 
-      def self.run_validator(value, params={})
-        raise ArgumentError, "Missing :legth parameter for Length validator" unless params.key? :length
-        raise ArgumentError, "The :length parameter has to be provided as a Range, #{params[:length].class.name} given" unless params[:length].is_a?(::Range)
+      protected
+        def self.run_validator(value, params={})
+          raise ArgumentError, "Missing :legth parameter for Length validator" unless params.key? :length
+          raise ArgumentError, "The :length parameter has to be provided as a Range, #{params[:length].class.name} given" unless params[:length].is_a?(::Range)
 
-        min_length = params[:length].min
-        max_length = params[:length].max
+          min_length = params[:length].min
+          max_length = params[:length].max
 
-        if value.respond_to? :length
-          if value.length < min_length
-            set_error :short, min_length
-            false
-          elsif value.length > max_length
-            set_error :long, max_length
-            false
+          if value.respond_to? :length
+            if value.length < min_length
+              set_error :short, min_length
+              false
+            elsif value.length > max_length
+              set_error :long, max_length
+              false
+            else
+              true
+            end
           else
             true
           end
-        else
-          true
         end
-      end
+      # end Protected
     end
   end
 end
--- lib/mandrake/validator/presence.rb
+++ lib/mandrake/validator/presence.rb
@@ -7,17 +7,19 @@ module Mandrake
         :empty => "cannot be empty"
       }
 
-      def self.run_validator(value, params={})
-        if value.nil?
-          set_error :missing
-          false
-        elsif value.respond_to?(:empty?) && value.empty?
-          set_error :empty
-          false
-        else
-          true
+      protected
+        def self.run_validator(value, params={})
+          if value.nil?
+            set_error :missing
+            false
+          elsif value.respond_to?(:empty?) && value.empty?
+            set_error :empty
+            false
+          else
+            true
+          end
         end
-      end
+      # end Protected
     end
   end
 end
