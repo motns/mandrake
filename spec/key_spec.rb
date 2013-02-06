adb22a5d24289add50a94a779258cc3f08220c35
Adam Borocz <boroczadam@gmail.com>
2013-02-06 12:47:20 +0000

    Few more specs for Key object

--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -3,6 +3,53 @@ require 'spec_helper'
 describe Mandrake::Key do
 
   context "::initialize" do
+    context "when called with a valid name and type" do
+      context "and no options" do
+        before(:all) do
+          @key_object = described_class.new(:title, :String)
+        end
+
+        it "sets :required to false" do
+          @key_object.required.should be_false
+        end
+
+        it "sets :default to nil" do
+          @key_object.default.should be_nil
+        end
+      end
+
+
+      context "and :required = true" do
+        before(:all) do
+          @key_object = described_class.new(:title, :String, required: true)
+        end
+
+        it "sets :required to true" do
+          @key_object.required.should be_true
+        end
+
+        it "sets :default to nil" do
+          @key_object.default.should be_nil
+        end
+      end
+
+
+      context "and :default as a scalar" do
+        before(:all) do
+          @key_object = described_class.new(:title, :String, default: 123)
+        end
+
+        it "sets :required to false" do
+          @key_object.required.should be_false
+        end
+
+        it "sets :default to scalar value" do
+          @key_object.default.should eq(123)
+        end
+      end
+    end
+
+
     context "when called with a type parameter that can't be converted to a Symbol" do
       it "throws an exception" do
         expect {
