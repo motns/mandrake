7c313ec90476b4fc570b3d960cf3dc1f1052e5ee
Adam Borocz <boroczadam@gmail.com>
2013-02-14 15:11:20 +0000

    Documentation for Boolean Type

--- lib/mandrake/type/boolean.rb
+++ lib/mandrake/type/boolean.rb
@@ -1,6 +1,11 @@
 module Mandrake
   module Type
+    # Type class for storing Boolean values (shocking!)
   	class Boolean < Base
+      # Sets new value - nil is preserved, otherwise it casts into TrueClass/FalseClass
+      #
+      # @param val New value
+      # @ return [TrueClass, FalseClass, nil] The updated value
       def value=(val)
         @value = val.nil? ? nil : !!val
       end
