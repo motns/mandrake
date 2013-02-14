8f46b250fb057995df216a0b17c2c358530baf3a
Adam Borocz <boroczadam@gmail.com>
2013-02-14 15:11:11 +0000

    Code cleanup and documentation for String Type

--- lib/mandrake/type/string.rb
+++ lib/mandrake/type/string.rb
@@ -1,10 +1,23 @@
 module Mandrake
   module Type
+    # Type class for storing textual values. Lenght is restricted to 50 characters
+    # by default, but this can be overridden. There are no default restrictions
+    # on format.
   	class String < Base
+
+      # Default parameters for this Type, processed by {Mandrake::Type::Base.params}
       PARAMS = {length: 0..50, format: nil}
 
+
+      # Sets new value - nil is preserved, otherwise it casts into String via to_s
+      #
+      # @param val New value
+      # @ return [String, nil] The updated value
       def value=(val)
-        @value = val.nil? ? nil : val.to_s
+        @value = if val.nil? then nil
+                 elsif val.respond_to?(:to_s) then val.to_s
+                 else nil
+                 end
       end
     end
 	end
