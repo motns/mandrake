e2e0845e4a70f472dee700265c14e0fc90c6ac42
Adam Borocz <boroczadam@gmail.com>
2013-02-19 19:22:28 +0000

    Grouped results for code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 require 'simplecov'
 SimpleCov.start do
   add_filter "spec/"
+  add_group "Components", "lib/mandrake/components"
+  add_group "Types", "lib/mandrake/type"
+  add_group "Validators", "lib/mandrake/validator"
 end
 
 require 'mandrake'
