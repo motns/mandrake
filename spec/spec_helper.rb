dd8ae6cc73ab0ab5c390e86a6a47c6654b2b0140
Adam Borocz <boroczadam@gmail.com>
2013-03-01 19:02:14 +0000

    Exclude bundler gems from code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'simplecov'
 SimpleCov.start do
   add_filter "spec/"
+  add_filter "vendor/"
   add_group "Components", "lib/mandrake/components"
   add_group "Types", "lib/mandrake/type"
   add_group "Validators", "lib/mandrake/validator"
