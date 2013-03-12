9471e1762801c0c28610e57444137046a392bab1
Adam Borocz <boroczadam@gmail.com>
2013-03-12 18:57:33 +0000

    Documentation for error class

--- lib/mandrake/errors.rb
+++ lib/mandrake/errors.rb
@@ -1,6 +1,9 @@
 module Mandrake
+  # Internal Mandrake error classes
   module Error
+    # Used when the name for a key that's being defined is not valid
     class KeyNameError < StandardError; end
+    # Used when the alias for a key that's being defined is not valid
     class KeyAliasError < StandardError; end
   end
 end
